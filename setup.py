dcf75cb6f5a798e762551d995ce371a4251112be
ahmadfauzy02 <ahmadfauzyarif@gmail.com>
2025-01-30 10:30:01 +0800

    add license

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
-
-
-import os
-import sys
-import obspy
-import numpy as np
-from obspy import read
-import pandas
-import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
-from matplotlib.cm import get_cmap
-from scipy import signal
-from scipy.fft import fft, fftfreq  # Removed duplicate fft/fftfreq import
-from matplotlib.ticker import LogLocator, FixedLocator, MultipleLocator, LogFormatter
-from scipy.signal import decimate
-from scipy.special import jv
-from scipy.optimize import least_squares
-import scipy
-    
 from setuptools import setup, find_packages
 import codecs
 import os
@@ -24,30 +5,30 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
+    long_description = fh.read()
 
-VERSION = '0.0.1'
+VERSION = '0.1'
 DESCRIPTION = 'Spatial Autocorrelation Processing'
-LONG_DESCRIPTION = 'Package for extract dispersion curve based on spatial autocorrelation method.'
 
-# Setting up
 setup(
     name="spacunhas",
     version=VERSION,
     author="Ahmad Fauzy",
-    author_email="<ahmadfauzyarif@gmail.com>",
+    author_email="ahmadfauzyarif@gmail.com",
     description=DESCRIPTION,
-    long_description_content_type="text/markdown",
     long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/spacunhas",
+    license="MIT",
     packages=find_packages(),
-    install_requires=['scipy', 'obspy', 'pandas', 'numpy', 'matplotlib', 'os'],
+    install_requires=['scipy', 'obspy', 'pandas', 'numpy', 'matplotlib'],
     keywords=['geophysics', 'dispersioncurve', 'spac'],
     classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Geophysicst",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Linux",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Geophysicists",
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: OS Independent", 
+    ],
+    python_requires='>=3.12',  
 )
+
