46d8f04c86606d244bc9ec602c3b6011baa85d7f
hjibran <haidirjibran@gmail.com>
2025-02-01 21:43:32 +0800

    Perbaikan minor: 1) Mengubah path folder pada directrun.ipynb agar lebih umum. 2) Mengubah penggunaan tanda kutip dalam tanda kutip pada environment.py dan windowing.py

--- spacunhas/environment.py
+++ spacunhas/environment.py
@@ -30,8 +30,8 @@ class SPACProcessing():
     def __init__(self, path):
         self.path = path
         self.path_data= f'{os.path.join(self.path, "data")}/'
-        self.path_processing= f'{os.path.join(self.path, 'processing')}/'
-        self.path_figures= f'{os.path.join(self.path, 'figures')}/'
+        self.path_processing= f'{os.path.join(self.path, "processing")}/'
+        self.path_figures= f'{os.path.join(self.path, "figures")}/'
         self.make_folder()
         
     def make_folder(self):
